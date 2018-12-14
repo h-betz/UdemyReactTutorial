a18da96870a5135e33181bb42314e62c9ae10b3f
Hunter Betz <hhb311@gmail.com>
2018-12-13 20:46:57 -0500

    Adding seasons project

---
+++ seasons/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+
+class App extends React.Component {
+
+    state = { lat: null, errorMessage: '' };
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    componentDidMount()  {
+        window.navigator.geolocation.getCurrentPosition(
+            position => 
+                this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+            
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it re-rendered');
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please accept the location request" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
