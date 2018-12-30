0813509d2459cc37dd7f0a12946d350740a37da7
Hunter Betz <hhb311@gmail.com>
2018-12-30 14:46:50 -0500

    Making api requests

--- pics/pics/src/components/App.js
+++ pics/pics/src/components/App.js
@@ -1,12 +1,27 @@
+import axios from 'axios';
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID ae76c99633e8820c54107865c51f055f04c1cc5599b1a75f996340dd921628d0'
+            }
+        });
+    }
+    
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
+}
 
 export default App;
