e4cfe9d5e0d417cc69fe54a39ba1435131318b30
Hunter Betz <hhb311@gmail.com>
2019-02-21 07:06:56 -0500

    New project

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            <Link to="/pagetwo">Navigate to Page Two</Link>
+        </div>
+    );
+};
+
+const PageTwo = () => {
+    return (
+        <div>
+            PageTwo
+            <button>Click Me!</button>
+            <Link to="/">Navigate to Page One</Link>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
