f5151765b162382f03678b590cf46f9c02d07780
Hunter Betz <hhb311@gmail.com>
2019-02-27 20:46:24 -0500

    Working on redux reducers

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 //Function that returns a function
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-    dispatch({type: 'FETCH_POSTS', payload: response})
+    dispatch({type: 'FETCH_POSTS', payload: response});
 };
